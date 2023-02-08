6f9c19d2a3cf4a1fa016fcbc3206fd14c4faf84f
Sefa Emre Kavgacı <emre.kavgaci@ug.bilkent.edu.tr>
2023-02-08 11:19:03 +0300

    a

---
+++ Colaboration.java
@@ -0,0 +1,16 @@
+public class Colaboration {
+    public int evenSum(int[] array) {
+        int result = 0;
+        for(int i = 0; i < array.length / 2; i++ ){
+            result += array[2*i + 1];
+        }
+        return result;
+    }
+    public int oddSum(int[] array){
+        int result = 0;
+        for(int i = 0; i < array.length / 2.0; i++ ){
+            result += array[2*i];
+        }
+        return result;
+    }
+}
