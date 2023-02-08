74ccede2724bc869e29ede201d457859543d1e99
Ömer Bıçakcıoğlu <omer.bicakcioglu@ug.bilkent.edu.tr>
2023-02-08 11:17:23 +0300

    yes

--- CreateAnArray.java
+++ CreateAnArray.java
@@ -1,23 +1,17 @@
 import java.util.Random;
 import java.util.Scanner;
 
-
 public class CreateAnArray {
     public static int[] createArray() {
         Scanner in = new Scanner(System.in);
         System.out.print("Please enter an array lenght: ");
         Random random = new Random();
-        int lengthOfArray = in.nextInt(); 
-        int[] array = new int[10];
+        int lengthOfArray = in.nextInt();
+        int[] array = new int[lengthOfArray];
         for (int i = 0; i < array.length; i++) {
             array[i] = random.nextInt(100);
         }
         return array;
     }
-    public static void main(String[] args) {
-        int[] array = createArray();
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i] + " ");
-        }
-    }
+
 }
--- omer.java
+++ omer.java
@@ -1,29 +1,31 @@
 import java.util.Scanner;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class omer {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Please select your option: ");
-        System.out.println("1- Create an array");
-        System.out.println("2- Find min and max of an array");
-        System.out.println("3- Find the average of an array");
-        System.out.println("4- Display how each element of the array differs from the average");
-        System.out.println("5- Find the sum of elements with odd- and even-numbered indexes");
-        System.out.println("6- Exit ");
-        int option = in.nextInt();
         boolean isDone = false;
+        int[] newArray = CreateAnArray.createArray();
+        for (int i = 0; i < newArray.length - 1; i++) {
+            System.out.print(newArray[i] + ", ");
+        }
+        System.out.println(newArray[newArray.length - 1]);
 
         while (!isDone) {
+            System.out.println("Please select your option: ");
+            System.out.println("1- Find min and max of an array");
+            System.out.println("2- Find the average of an array");
+            System.out.println("3- Display how each element of the array differs from the average");
+            System.out.println("4- Find the sum of elements with odd- and even-numbered indexes");
+            System.out.println("5- Exit ");
+            int option = in.nextInt();
             if (option == 6) {
                 System.out.println("Program has finished.");
                 isDone = true;
-            }
-            if (option == 1) {
-                int[] newArray = CreateAnArray.createArray();
-                for (int i = 0; i < newArray.length - 1; i++) {
-                    System.out.print(newArray[i] + ", ");
-                }
-                System.out.print(newArray[newArray.length - 1]);
+            } else if (option == 1) {
+                System.out.println("Min value in array: " + assignment.findMin(newArray));
+                System.out.println("Max value in array: " + assignment.findMax(newArray));
 
             }
         }
